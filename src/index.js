b9c472e50d612034197ee06210302b55e25c9212
adeleaulia <adel@artotelgroup.com>
2026-05-31 17:51:02 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Use "type: module" in package.json to use ES modules
+import express from 'express';
+const app = express();
+const port = 3000;
+ 
+// Define your routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
